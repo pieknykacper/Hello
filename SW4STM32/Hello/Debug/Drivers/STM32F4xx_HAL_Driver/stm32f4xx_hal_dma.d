306366955d5897cb61f18777a505b4cc4f0cf123
pieknykacper <buszmen127@wp.pl>
2017-06-06 14:48:39 +0200

    STM

---
+++ SW4STM32/Hello/Debug/Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_dma.d
@@ -0,0 +1,77 @@
+Drivers/STM32F4xx_HAL_Driver/stm32f4xx_hal_dma.o: \
+ C:/Users/Kacper/Desktop/STM32/Hello/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c \
+ C:/Users/Kacper/Desktop/STM32/Hello/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ C:/Users/Kacper/Desktop/STM32/Hello/Inc/stm32f4xx_hal_conf.h \
+ C:/Users/Kacper/Desktop/STM32/Hello/Inc/main.h \
+ C:/Users/Kacper/Desktop/STM32/Hello/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ C:/Users/Kacper/Desktop/STM32/Hello/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ C:/Users/Kacper/Desktop/STM32/Hello/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ C:/Users/Kacper/Desktop/STM32/Hello/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xc.h \
+ C:/Users/Kacper/Desktop/STM32/Hello/Drivers/CMSIS/Include/core_cm4.h \
+ C:/Users/Kacper/Desktop/STM32/Hello/Drivers/CMSIS/Include/core_cmInstr.h \
+ C:/Users/Kacper/Desktop/STM32/Hello/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/Kacper/Desktop/STM32/Hello/Drivers/CMSIS/Include/core_cmFunc.h \
+ C:/Users/Kacper/Desktop/STM32/Hello/Drivers/CMSIS/Include/core_cmSimd.h \
+ C:/Users/Kacper/Desktop/STM32/Hello/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ C:/Users/Kacper/Desktop/STM32/Hello/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ C:/Users/Kacper/Desktop/STM32/Hello/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ C:/Users/Kacper/Desktop/STM32/Hello/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ C:/Users/Kacper/Desktop/STM32/Hello/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ C:/Users/Kacper/Desktop/STM32/Hello/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ C:/Users/Kacper/Desktop/STM32/Hello/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ C:/Users/Kacper/Desktop/STM32/Hello/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ C:/Users/Kacper/Desktop/STM32/Hello/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ C:/Users/Kacper/Desktop/STM32/Hello/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ C:/Users/Kacper/Desktop/STM32/Hello/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ C:/Users/Kacper/Desktop/STM32/Hello/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ C:/Users/Kacper/Desktop/STM32/Hello/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h
+
+C:/Users/Kacper/Desktop/STM32/Hello/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+
+C:/Users/Kacper/Desktop/STM32/Hello/Inc/stm32f4xx_hal_conf.h:
+
+C:/Users/Kacper/Desktop/STM32/Hello/Inc/main.h:
+
+C:/Users/Kacper/Desktop/STM32/Hello/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+
+C:/Users/Kacper/Desktop/STM32/Hello/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+
+C:/Users/Kacper/Desktop/STM32/Hello/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+
+C:/Users/Kacper/Desktop/STM32/Hello/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f401xc.h:
+
+C:/Users/Kacper/Desktop/STM32/Hello/Drivers/CMSIS/Include/core_cm4.h:
+
+C:/Users/Kacper/Desktop/STM32/Hello/Drivers/CMSIS/Include/core_cmInstr.h:
+
+C:/Users/Kacper/Desktop/STM32/Hello/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+C:/Users/Kacper/Desktop/STM32/Hello/Drivers/CMSIS/Include/core_cmFunc.h:
+
+C:/Users/Kacper/Desktop/STM32/Hello/Drivers/CMSIS/Include/core_cmSimd.h:
+
+C:/Users/Kacper/Desktop/STM32/Hello/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+C:/Users/Kacper/Desktop/STM32/Hello/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+C:/Users/Kacper/Desktop/STM32/Hello/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+
+C:/Users/Kacper/Desktop/STM32/Hello/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+
+C:/Users/Kacper/Desktop/STM32/Hello/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+
+C:/Users/Kacper/Desktop/STM32/Hello/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+
+C:/Users/Kacper/Desktop/STM32/Hello/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+
+C:/Users/Kacper/Desktop/STM32/Hello/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+
+C:/Users/Kacper/Desktop/STM32/Hello/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+
+C:/Users/Kacper/Desktop/STM32/Hello/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+
+C:/Users/Kacper/Desktop/STM32/Hello/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+C:/Users/Kacper/Desktop/STM32/Hello/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+
+C:/Users/Kacper/Desktop/STM32/Hello/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
